a2200541b9ce0c897ffb0fa9741a4defd4c7186a
Nathan <nathan.guimaraes@inf.ufrgs.br>
2025-07-28 19:27:10 -0300

    better inheritage for hash tables

--- include/hashtable.cpp
+++ include/hashtable.cpp
@@ -2,14 +2,9 @@
 #include <string>
 #include <vector>
 #include "hashtable.hpp"
-using std::to_string;
 
-HashPlayer::HashPlayer(){
-    for(int i = 0; i < HASH_SIZE ; i++){
-        hash_table.push_back(vector<Player>());
-    }
-}
-int HashPlayer::getKey(string id, int size){
+// HashPlayer implementation
+int HashPlayer::getKey(const string& id, int size){
     long long int hash_key = 0;
     int a = 31;
 
@@ -21,31 +16,9 @@ int HashPlayer::getKey(string id, int size){
 
     return hash_key;
 }
-void HashPlayer::insert(Player player)
-{
-    int key = getKey(player.getId(), player.getId().size());
-    hash_table[key].push_back(player);        
-}
-Player* HashPlayer::search(string id){
-    int size = id.size();
-    int key = getKey(id, size);
 
-    for(size_t i = 0; i < hash_table[key].size(); i++){
-        if(hash_table[key][i].getId() == id)
-            return &hash_table[key][i];
-    }
-
-    return NULL;    
-}
-
-
-
-HashUser::HashUser(){
-    for(int i = 0; i < HASH_SIZE ; i++){
-        hash_table.push_back(vector<User>());
-    }
-}
-int HashUser::getKey(string id, int size){
+// HashUser implementation  
+int HashUser::getKey(const string& id, int size){
     long long int hash_key = 0;
     int a = 31;
 
@@ -56,29 +29,4 @@ int HashUser::getKey(string id, int size){
     hash_key %= HASH_SIZE;
 
     return hash_key;
-}
-void HashUser::insert(User user){
-    int key = getKey(user.getId(), user.getId().size());
-
-    for(size_t i = 0; i < hash_table[key].size(); i++)
-    {
-        if(hash_table[key].at(i).getId() == user.getId())
-        {
-            hash_table[key].at(i).insertRatedPlayer(user.getRatedPlayerBuffer());
-            return;
-        }
-    }   
-    user.insertRatedPlayer(user.getRatedPlayerBuffer());
-    hash_table[key].push_back(user);
-}
-User* HashUser::search(string id){
-    int size = id.size();
-    int key = getKey(id, size);
-
-    for(size_t i = 0; i < hash_table[key].size(); i++){
-        if(hash_table[key][i].getId() == id)
-            return &hash_table[key][i];
-    }
-
-    return NULL;    
 }
--- include/hashtable.hpp
+++ include/hashtable.hpp
@@ -7,35 +7,100 @@
 #include "user.hpp"
 using std::string, std::vector;
 
-// base hash class
+// base hash table template
+template<typename T>
 class HashTable{
 protected:
+    vector<vector<T>> hash_table;
+    int HASH_SIZE;
+    
+    // get hash key for string ID
+    virtual int getKey(const string& id, int size) = 0;
+    
+public:
+    HashTable(int size) : HASH_SIZE(size) {
+        hash_table.resize(HASH_SIZE);
+    }
+    
+    virtual ~HashTable() = default;
+    
+    // insert item into hash table
+    virtual void insert(T& item) {
+        int key = getKey(item.getId(), item.getId().size());
+        
+        // check if item exists
+        for(size_t i = 0; i < hash_table[key].size(); i++) {
+            if(hash_table[key][i].getId() == item.getId()) {
+                handleExistingItem(hash_table[key][i], item);
+                return;
+            }
+        }
+        
+        // add new item
+        prepareNewItem(item);
+        hash_table[key].push_back(item);
+    }
+    
+    // search for item by ID
+    T* search(const string& id) {
+        int size = id.size();
+        int key = getKey(id, size);
+
+        for(size_t i = 0; i < hash_table[key].size(); i++) {
+            if(hash_table[key][i].getId() == id)
+                return &hash_table[key][i];
+        }
+
+        return nullptr;
+    }
+    
+protected:
+    // handle duplicate items
+    virtual void handleExistingItem(T& existing, T& newItem) = 0;
+    
+    // prepare item before insertion
+    virtual void prepareNewItem(T& item) = 0;
 };
 
-
-// player hash (inherit from base hash)
-class HashPlayer: public HashTable{
+// player hash table
+class HashPlayer: public HashTable<Player>{
 private:
-    vector<vector<Player>> hash_table;
-    const int HASH_SIZE = 1.5*18944;
-    int getKey(string id, int size);
+    int getKey(const string& id, int size) override;
+    
+protected:
+    void handleExistingItem(Player& existing, Player& newItem) override {
+        // ignore duplicates
+        (void)existing;
+        (void)newItem;
+    }
+    
+    void prepareNewItem(Player& item) override {
+        // no preparation needed
+        (void)item;
+    }
+    
 public:
-    void insert(Player player);
-    Player* search(string id);
-    HashPlayer();
+    HashPlayer() : HashTable<Player>(static_cast<int>(1.5 * 18944)) {}
 };
 
-
-// user hash (inherit from base hash)
-class HashUser: public HashTable{
+// user hash table
+class HashUser: public HashTable<User>{
 private:
-    vector<vector<User>> hash_table;
-    const int HASH_SIZE = 1.3*24188078;
-    int getKey(string id, int size);
+    int getKey(const string& id, int size) override;
+    
+protected:
+    void handleExistingItem(User& existing, User& newItem) override {
+        // merge rated players
+        existing.insertRatedPlayer(newItem.getRatedPlayerBuffer());
+    }
+    
+    void prepareNewItem(User& item) override {
+        // insert buffer before adding
+        item.insertRatedPlayer(item.getRatedPlayerBuffer());
+    }
+    
 public:
-    void insert(User user);
-    User* search(string id);
-    HashUser();
+    HashUser() : HashTable<User>(static_cast<int>(1.3 * 24188078)) {}
 };
 
 #endif
